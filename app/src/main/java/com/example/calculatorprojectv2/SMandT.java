895a46135a51586993d44cfc88e508b8cbbd2168
ianeu <ianeunjoon@hotmail.com>
2021-09-08 09:04:35 -0500

    fgd

---
+++ app/src/main/java/com/example/calculatorprojectv2/SMandT.java
@@ -0,0 +1,14 @@
+package com.example.calculatorprojectv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SMandT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smand_t);
+    }
+}
